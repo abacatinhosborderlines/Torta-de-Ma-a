f67a8a2bbad9fe6776411a5ab60196344488de91
abacatinhosborderlines <161480073+abacatinhosborderlines@users.noreply.github.com>
2024-09-26 12:23:39 -0300

    sdasd

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 alert('Maria Paula e Luane Mendes')
-let numero=parseInt(prompt('Insera um numero'))
-let um=parseInt(prompt('Qual sua nota no 1 trimestre'))
-let dois=parseInt(prompt('Qual sua nota no 2 trimestre'))
-let tres=parseInt(prompt('Qual sua nota no 3 trimestre'))
+let numero = parseInt(prompt('Insera um numero'))
+let um = parseInt(prompt('Qual sua nota no 1 trimestre'));
+let dois = parseInt(prompt('Qual sua nota no 2 trimestre'));
+let tres = parseInt(prompt('Qual sua nota no 3 trimestre'));
 
-function soma(numero1,numero2){return numero1+numero2;}
-console.log(soma(1,2));
-function areaQuadrado(lado){return lado**2;}
-console.log(areaQuadrado(numero))
-let media=function(um,dois,tres) 
-{return um+dois+tres}
+function soma(numero1, numero2) { return numero1 + numero2; }
+console.log(soma(1, 2));
+function areaQuadrado(lado) { return lado ** 2; }
+console.log(areaQuadrado(numero));
+let media = function (um, dois, tres) {
+    return (um + dois + tres)/3
+}
 
+document.write(media(um,dois,tres))
