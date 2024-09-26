7f389d375a1d4e9268b443c4c52b168f84fbeae7
abacatinhosborderlines <161480073+abacatinhosborderlines@users.noreply.github.com>
2024-09-26 11:40:53 -0300

    asda

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ alert('Maria Paula e Luane Mendes')
 function soma(numero1,numero2){return numero1+numero2;}
 console.log(soma(1,2));
 function areaQuadrado(lado){return lado**2;}
-console.log(areaQuadrado(2,2))
+console.log(areaQuadrado(2))
