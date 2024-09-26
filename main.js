08f5541418f9aa063a854d8d50b9857870606863
abacatinhosborderlines <161480073+abacatinhosborderlines@users.noreply.github.com>
2024-09-26 12:17:07 -0300

    asdasd

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 alert('Maria Paula e Luane Mendes')
 let numero=parseInt(prompt('Insera um numero'))
+let 1=parseInt(prompt('Qual sua nota no 1 trimestre'))
+let 2=parseInt(prompt('Qual sua nota no 2 trimestre'))
+let 3=parseInt(prompt('Qual sua nota no 3 trimestre'))
+
 function soma(numero1,numero2){return numero1+numero2;}
 console.log(soma(1,2));
 function areaQuadrado(lado){return lado**2;}
 console.log(areaQuadrado(numero))
-let media=function() 
-{}
+let media=function(1,2,3) 
+{return 1+2+3}
 
