82045f0500bbcad258957bee6579946f2596d6d7
abacatinhosborderlines <161480073+abacatinhosborderlines@users.noreply.github.com>
2024-09-26 11:33:29 -0300

    asdasd

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 alert('Maria Paula e Luane Mendes')
 function soma(numero1,numero2){return numero1+numero2;}
 console.log(soma(1,2));
+function potenciação(numero1,2){return numero1**2;}
+console.log(potenciação(2))
