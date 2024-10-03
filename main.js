11f070ddfc879542b2993a7ab693acc44f2ef08c
abacatinhosborderlines <161480073+abacatinhosborderlines@users.noreply.github.com>
2024-10-03 11:24:54 -0300

    klmok

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-alert('Maria Paula e Luane Mendes')
-let numero = parseInt(prompt('Insera um numero'))
-let um = parseInt(prompt('Qual sua nota no 1 trimestre'));
-let dois = parseInt(prompt('Qual sua nota no 2 trimestre'));
-let tres = parseInt(prompt('Qual sua nota no 3 trimestre'));
+// alert('Maria Paula e Luane Mendes')
+// let numero = parseInt(prompt('Insera um numero'))
+// let um = parseInt(prompt('Qual sua nota no 1 trimestre'));
+// let dois = parseInt(prompt('Qual sua nota no 2 trimestre'));
+// let tres = parseInt(prompt('Qual sua nota no 3 trimestre'));
 
-function soma(numero1, numero2) { return numero1 + numero2; }
-console.log(soma(1, 2));
-function areaQuadrado(lado) { return lado ** 2; }
-console.log(areaQuadrado(numero));
-let media = function (um, dois, tres) {
-    return (um + dois + tres)/3
-}
+// function soma(numero1, numero2) { return numero1 + numero2; }
+// console.log(soma(1, 2));
+// function areaQuadrado(lado) { return lado ** 2; }
+// console.log(areaQuadrado(numero));
+// let media = function (um, dois, tres) {
+//     return (um + dois + tres)/3
+// }
 
-document.write(media(um,dois,tres))
+// document.write(media(um,dois,tres))
+
+let Estados=[
+    'Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Espírito Santo', 'Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins','Distrito Federal']
