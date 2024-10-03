e7a86ae47e2d5a780a0cd4fb0e2fcd95a0ea5246
abacatinhosborderlines <161480073+abacatinhosborderlines@users.noreply.github.com>
2024-10-03 11:26:27 -0300

    asda

--- main.js
+++ main.js
@@ -15,5 +15,8 @@
 // document.write(media(um,dois,tres))
 
 let Estados=[
-    'Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Espírito Santo', 'Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins','Distrito Federal']
+    'Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Espírito Santo', 'Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba',
+    'Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins',
+    'Distrito Federal'] ;
+    
 console
